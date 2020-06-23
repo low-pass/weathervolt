ffbe34bebdd82397aedc8534937732ee17834309
low-pass <kirisouge@gmail.com>
2020-06-24 00:00:38 +0300

    service logic, pip dependencies

--- src/service.py
+++ src/service.py
@@ -1,13 +1,35 @@
 from util import *
 from pygame import mixer
+from datetime import datetime
 import time
+import math
+import ntplib
 
 mixer.init()
 
-highest = 30
-lowest = 0
-wav_output(highest,lowest,'a')
+id_char = 'a'
 
 while True:
-    time.sleep(10)
+    c = ntplib.NTPClient()
+    req = c.request('europe.pool.ntp.org', version=3)
+    timenow = datetime.fromtimestamp(req.tx_time)
+    highest, lowest, status = get_forecast(timenow)
+    print('Time: ' + str(timenow) + ' OWM status: ' + status)
+
+    if status == 'owmfail':
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        with open(dir_path + "/../wavecfg.yaml", 'r') as stream:
+            wavecfg = yaml.safe_load(stream)
+        fullscale = wavecfg['fullsc']
+        wav_output(fullscale/2,fullscale/2,id_char)
+    else:
+        print('Highest temp: ' + str(highest) + ' Lowest temp: ' + str(lowest))
+        wav_output(highest,lowest,id_char)
+
+    if id_char == 'a':
+        id_char = 'b'
+    else:
+        id_char = 'a'
+
+    time.sleep(600)
 
